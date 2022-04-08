f4366bf11f771ca7b87bc1dc760351e65c5a1d4a
Luis E. Bobadilla Dias <superluis5@gmail.com>
2022-04-07 20:23:52 -0400

    day 1 finished

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,3 +1,38 @@
-# Printing, Commenting, Debugging, String Manipulation and Variables.
+# Day 1 - Beginner - Working with Variables in Python to Manage Data
 
 print("Hello world!")
+
+print("Hello world!\nHello world!\nHello World!")
+
+print("Hello " + "Angela")
+
+print("What is your name?")
+
+input("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+# This is a comment
+
+name = input("What is your name?")
+
+print(name)
+
+name = "Jack"
+
+print(name)
+
+name = "Angela"
+
+print(name)
+
+print(len(input("What is your name?")))
+
+name = input("What is your name?")
+
+length = len(name)
+
+print(length)
+
+# Rules for variable naming
+# Make your code readable, _ to separate words, and can't start with a number. Certain words are also not allowed like print or input. 
