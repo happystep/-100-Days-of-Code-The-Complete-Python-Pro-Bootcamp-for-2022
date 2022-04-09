0301cf520fe0721a3dcf0f1cc6e285901dbb46ba
Luis E. Bobadilla Dias <superluis5@gmail.com>
2022-04-09 14:18:04 -0400

    day 3 finished

---
+++ Day3/day3.py
@@ -0,0 +1,88 @@
+# Day 3 - Beginner - Control Flow and Logical Operators
+
+print("Welcome to the Rollercoaster")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# Comparison Operators
+
+# >
+# <
+# >=
+# <=
+# ==
+# != 
+
+# Nested if statements and elif statements
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+    age  = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $7")
+    elif age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# Multiple if statements in succession
+
+bill = 0
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+    age  = int(input("What is your age? "))
+    if age < 12:
+        print("Children tickets are $7")
+        bill = 12
+    elif age <= 18:
+        print("Youth tickets are $7")
+        bill = 7
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on ")
+    else:
+        print("Adult tickets are $12")
+        bill = 12
+    wants_photo = input("Do you wanta photo taken? Y or N.")
+    if wants_photo == 'Y':
+        bill += 3
+    print(f"Your final bill is {bill}")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# Logical Operators
+
+# and
+# or
+# not
+
+
+bill = 0
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+    age  = int(input("What is your age? "))
+    if age < 12:
+        print("Children tickets are $7")
+        bill = 12
+    elif age <= 18:
+        print("Youth tickets are $7")
+        bill = 7
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on ")
+    else:
+        print("Adult tickets are $12")
+        bill = 12
+    wants_photo = input("Do you wanta photo taken? Y or N.")
+    if wants_photo == 'Y':
+        bill += 3
+    print(f"Your final bill is {bill}")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
